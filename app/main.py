aa3c2fbbe34028f63120fa1c35daa0dafc5e4698
frwhatsgoingon-dev <frwhatsgoingon@gmail.com>
2026-03-04 10:55:18 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+# IMPORTANT: because your templates are inside the app folder:
+templates = Jinja2Templates(directory="app/templates")
+
 @app.get("/", response_class=HTMLResponse)
-def home():
-    return """
-    <h1>frwhatsgoingon</h1>
-    <p>Breaking news explained for students.</p>
-    """
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
